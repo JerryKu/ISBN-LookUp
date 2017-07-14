25f614ce8c5f6f44923a72a30d83cd0a1bedeb0a
JerryKu <jerryku1220@gmail.com>
2017-07-14 01:26:42 -0700

    added non vanillajs solution

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
-/**
- * index.js
- * - All our useful JS goes here, awesome!
- */
+//THIS SECTION IS DONE WITH JQUERY
+//---------------------------------------------------------------------------------
+const search = document.getElementById('search');
+
+search.addEventListener('click', function(){
+  isbn_search();
+});
+
 function isbn_search(){
   var ISBN = $("#query")[0].value;
   var query = "https://openlibrary.org/api/books?bibkeys=ISBN:" + ISBN + "&format=jsonp&jscmd=data";
@@ -23,4 +27,40 @@ function isbn_search(){
   });
 }
 
+//THIS SECTION IS DONE WITH VANILLAJS
+//----------------------------------------------------------------------------------------
+// const thumbnail = document.querySelector("#thumbnail");
+// const author = document.querySelector("#author");
+// const title = document.querySelector("#title");
+// const published = document.querySelector("#published");
+// const search = document.getElementById('search');
 
+// search.addEventListener('click', function(){
+//   isbn_search();
+// });
+
+
+// function myjson(data) {
+//   var ISBN = document.querySelector("#query").value;
+//   var obj = 'ISBN:' + ISBN;
+//   var authorData = data[obj].authors[0].name;
+//   var img = data[obj].cover.large;
+//   var titleData = data[obj].title;
+//   var publishedData = data[obj].publish_date;
+//   thumbnail.innerHTML = '<img src='+img +'>';
+//   title.innerHTML= '<h2>Title: '+ titleData + '</h2>';
+//   author.innerHTML = '<h2>Author: ' + authorData + '</h2>';
+//   published.innerHTML = '<h3>Published: '+ publishedData + '</h3>';
+
+
+// }
+
+// function isbn_search(){
+//   var ISBN = document.querySelector('#query').value;
+//   var query = "https://openlibrary.org/api/books?bibkeys=ISBN:" + ISBN + "&format=jsonp&jscmd=data&callback=myjson";
+//   var script = document.createElement('script');
+//   script.type = "text/javascript";
+//   script.src = query;
+//   document.head.appendChild(script);
+
+// }
